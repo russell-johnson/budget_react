fd5ecd3113401c5063feaded466a7071d92a8243
Russell Johnson <russellmax86@gmail.com>
2016-04-01 13:55:24 -0600

    small changes

--- app/assets/javascripts/components/bills/Bills.js.jsx
+++ app/assets/javascripts/components/bills/Bills.js.jsx
@@ -7,16 +7,17 @@ class bills extends React.Component  {
     return(
       <div className='row'>
           <div className= 'col s12 green darken-5'>
-            <form user money>
+            <form className
+            <input className=''>
           </div>
 
           <div className = 'container'>
             forms and table for bill pay
             <div className = 'forms for bills in  rows' >
-              
+
               <div className = 'row'>
                 <div className = 'bill price and name'>
-                here goes the form for a bill, and stuff (dont forget buttons for edit and delete)
+                {this.postBill()}
                 </div>
               </div>
 
@@ -24,7 +25,7 @@ class bills extends React.Component  {
                 <button onClick = 'submit' className ='btn blue'>Total</button>
                 <button onClick = 'create' className = 'btn green'>New Bill</button>
               </div>
-            
+
             </div>
 
             <div className = 'col s12 blue darken-5'>
