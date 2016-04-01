318907421885ec5a7e146d025855ac81368f4af2
jsquire4 <jacobmsquire@yahoo.com>
2016-04-01 12:07:02 -0600

    stubbed out view

--- app/assets/javascripts/components/bills/Bills.js.jsx
+++ app/assets/javascripts/components/bills/Bills.js.jsx
@@ -5,8 +5,33 @@ class bills extends React.Component  {
 
   render(){
     return(
-      <div className='center'>
-        hello world
+      <div className='row'>
+          <div className= 'col s12 green darken-5'>
+            <form user money>
+          </div>
+
+          <div className = 'container'>
+            forms and table for bill pay
+            <div className = 'forms for bills in  rows' >
+              
+              <div className = 'row'>
+                <div className = 'bill price and name'>
+                here goes the form for a bill, and stuff (dont forget buttons for edit and delete)
+                </div>
+              </div>
+
+              <div className = 'row'>
+                <button onClick = 'submit' className ='btn blue'>Total</button>
+                <button onClick = 'create' className = 'btn green'>New Bill</button>
+              </div>
+            
+            </div>
+
+            <div className = 'col s12 blue darken-5'>
+              in here goes the call for the math between the user money and the bill total
+            </div>
+          </div>
+
       </div>
     );
   }
